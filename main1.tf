19ae61087c3feabdb540041017f768e207446d57
rafeeqd <81919804+rafeeqd@users.noreply.github.com>
2021-04-23 20:29:15 +0530

    Add files via upload

---
+++ main1.tf
@@ -0,0 +1,19 @@
+
+# Terraform HCL
+
+provider "aws" {
+  region = "us-east-1"
+  #shared_credentials_file = "~/.aws/credentials"
+  #profile = "default"
+}
+
+
+resource "aws_vpc" "prodvpcterraform" {
+  cidr_block = "10.1.0.0/16"
+  enable_dns_support=true
+  enable_dns_hostnames = true
+  tags = {
+
+    Name = "VPC from Jenkins"
+}
+}
