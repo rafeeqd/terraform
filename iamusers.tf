9fd09a2c70c075c6c7d1506b336c8b72464fa27e
rafeeqd <81919804+rafeeqd@users.noreply.github.com>
2021-12-08 23:24:53 +0530

    Update iamusers.tf

--- iamusers.tf
+++ iamusers.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+      
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_file = "~/.aws/credentials"
+}
+
 resource "aws_iam_user" "name" {
   count = 3
   name = var.usernames[count.index]
