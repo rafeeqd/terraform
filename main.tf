4a251051af31cb95170b7b5a6eb06e6a7da0ee50
rafeeqd <81919804+rafeeqd@users.noreply.github.com>
2021-12-09 21:16:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-      
-    }
-  }
-}
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-  shared_credentials_file = "~/.aws/credentials"
-}
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
 
