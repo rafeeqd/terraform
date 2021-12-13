61b58352efe8a70a9f1793555589119e444584b0
rafeeqd <81919804+rafeeqd@users.noreply.github.com>
2021-12-13 18:32:01 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,19 @@
 variable "usernames" {
-type = list(string)
-default = [ "a" ,"b","c"]
+  type    = list(string)
+  default = ["a", "b", "c"]
+}
+
+variable "ami_to_select" {
+  type       = map(string)
+  descripton = " input the region and ami code"
+  default = {
+    "t2.micro" = "ami-0a1eddae0b7f0a79f" # first one
+    "t2.nano" = "ami-07d02ee1eeb0c996c" # Debian
+  }
+}
+
+variable "instance_type" {
+  type    = string
+  #default = "t2.micro"
+
 }
